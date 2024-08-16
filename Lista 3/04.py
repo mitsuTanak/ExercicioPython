d08b53a33c5c2b14d9e00e47642cc7d9e06f59b9
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-08-16 15:31:35 -0300

    a

---
+++ Lista 3/04.py
@@ -0,0 +1,13 @@
+print("")
+print("")
+print("")
+
+# var/input
+num1 = int(input("Insira o 1° Número: "))
+num2 = int(input("Insira o 2° Número: "))
+
+for i in range(0, 3):
+    total = num1+num2
+    i+=1
+    print(total)
+print(total*3)
